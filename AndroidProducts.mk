8d939556dd5cab5750bff91af6e5d89d2f4c2c9e
Erfan Abdi <erfangplus@gmail.com>
2022-08-04 01:42:32 +0430

    Adapt to LMODroid by device-add script
    Change-Id: I12ac7bf03237fa05a01075bd08d9dd719a44ea2e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_DRG.mk
+    $(LOCAL_DIR)/lmodroid_DRG.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_DRG-eng \
-    lineage_DRG-user \
-    lineage_DRG-userdebug
+    lmodroid_DRG-eng \
+    lmodroid_DRG-user \
+    lmodroid_DRG-userdebug
 
