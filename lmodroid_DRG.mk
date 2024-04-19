3e2c33cbed8943b9d5dc48a79ba68ca243848757
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:24:56 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_DRG.mk
+++ lmodroid_DRG.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -11,7 +11,7 @@ PRODUCT_BRAND := Nokia
 PRODUCT_DEVICE := DRG
 PRODUCT_MANUFACTURER := HMD Global
 PRODUCT_MODEL := Nokia 6.1 Plus
-PRODUCT_NAME := lineage_DRG
+PRODUCT_NAME := lmodroid_DRG
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=DRG_sprout \
